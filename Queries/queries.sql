9532398722581af14fff8b77d6cc90fedb1db056
Rebeca Osorio González <90414330+RebecaOsorio@users.noreply.github.com>
2021-11-25 01:40:36 -0600

    Create Tables and Visualize them

---
+++ Queries/queries.sql
@@ -0,0 +1,7 @@
+-- CONFIRM THAT THE TABLES ARE WELL CREATED
+SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+SELECT * FROM dept_employees;
+SELECT * FROM titles;
